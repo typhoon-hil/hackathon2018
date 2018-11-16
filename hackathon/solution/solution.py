1be2569c25905d5604c95c9b45670a58946c0541
mironiko87 <mironiko@gmail.com>
2018-11-16 17:42:47 +0100

    Dumb solution added (as a comment, below the dummy).

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -10,13 +10,42 @@ def worker(msg: DataMessage) -> ResultsMessage:
     """TODO: This function should be implemented by contestants."""
     # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
 
+    load_one = True
+    load_two = True
+    load_three = True
+    power_reference = 0.0
+    pv_mode = PVMode.ON
+
+    # if msg.grid_status:
+    #     if msg.bessSOC != 1 and msg.buying_price == msg.selling_price \
+    #             and msg.current_load <8:
+    #         if msg.bessSOC > 0.45:
+    #             power_reference = -5.0
+    #     elif msg.current_load > 8:
+    #         power_reference = msg.current_load
+    #
+    # else:
+    #     if msg.bessSOC == 1 and msg.solar_production > 0:
+    #         if msg.solar_production < msg.current_load:
+    #             power_reference = msg.current_load
+    #             load_three = False
+    #             pv_mode = PVMode.OFF
+    #         else:
+    #             power_reference = msg.current_load
+    #     elif msg.bessSOC != 1:
+    #         load_three = False
+    #         pv_mode = PVMode.ON
+    #
+    #     elif msg.selling_price == msg.buying_price and msg.solar_production == 0:
+    #         power_reference = msg.current_load
+
     # Dummy result is returned in every cycle here
     return ResultsMessage(data_msg=msg,
-                          load_one=True,
-                          load_two=True,
-                          load_three=True,
-                          power_reference=0.0,
-                          pv_mode=PVMode.ON)
+                          load_one=load_one,
+                          load_two=load_two,
+                          load_three=load_three,
+                          power_reference=power_reference,
+                          pv_mode=pv_mode)
 
 
 def run(args) -> None:
