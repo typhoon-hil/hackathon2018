b9d4b7ab973ddf87a3ba8ce020c6216e8d85e2f5
pegazbre <predrag@typhoon-hil.com>
2018-11-18 16:13:26 +0100

    Add teams

--- pull_and_run.py
+++ pull_and_run.py
@@ -4,6 +4,17 @@ import os
 import subprocess
 import shutil
 
+# Team 3 - https://github.com/mihajlolukic12/hackathonEnergy
+# Maker  - https://github.com/dusansimic/hackathon2018
+# py.yeet - https://github.com/Selich/TyphoonHackathon2018
+# Lambda - https://github.com/nmilosev/lphpub
+# CodeAmplifiers - https://github.com/SrdjanPaunovic/Hackathon2018
+# SWeet - https://github.com/phuskus/pyhackathon18.git
+# Zelena Jabuka - https://bitbucket.org/vladimirvincan/pythonhackathon2018/src/master/
+# CodeBanders - https://github.com/stevanmatovic/hakalaka2018
+# Data Vaders - https://gitlab.com/nemanja-m/hil-hackathon
+# K33 - https://github.com/stevorackovic/h333
+
 if __name__ == '__main__':
     theirs = 'hackathon2018.their'
     os.system('git reset --hard')
