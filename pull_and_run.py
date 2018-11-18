2d1ab02a20bd4d960786ae5d74b8c03876664334
pegazbre <predrag@typhoon-hil.com>
2018-11-18 14:17:14 +0100

    Update pull_and_run.py

--- pull_and_run.py
+++ pull_and_run.py
@@ -4,22 +4,8 @@ import os
 import subprocess
 import shutil
 
-# KoloTree https://github.com/gknezevic/solution.git
-# Tim404 https://github.com/stevanmatovic/hakalaka.git
-# AMiTeliHardware https://github.com/miloshunter/hackaton2017-AMiTeliHardware.git
-# Cobra Team https://github.com/djordje2192/hackthon2017.git
-# ~tilda~ https://github.com/tilda-center/hackathon2017.git
-# Team TRV https://github.com/to92me/hackathon2017.git
-# HilClimbers https://github.com/Djaps94/hackaton-2017.git
-# SyntaxError https://github.com/fivkovic/TyphoonHIL_Hackathon2017.git
-# Bit Please https://github.com/stek93/hackathon2017.git
-# PyLiman https://github.com/AirmiX/typhoonhil-hackathon2017.git
-# NoName https://github.com/bakaja021/typhoon_aip.git
-# Code10 https://github.com/lukamaletin/typhoon-hil-hackathon2017.git
-
-
 if __name__ == '__main__':
-    theirs = 'hackathon2017.their'
+    theirs = 'hackathon2018.their'
     os.system('git reset --hard')
     subprocess.run(['git', 'clone', sys.argv[1], theirs])
 
